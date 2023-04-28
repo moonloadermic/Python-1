821b621165e0f4fb50648f3f9f75af5f353c8034
362227 <91358231+362227@users.noreply.github.com>
2023-04-29 00:18:34 +0800

    Update Twitter.py

--- Twitter.py
+++ Twitter.py
@@ -7,6 +7,8 @@ import time
 import re
 from datetime import datetime
 from pyvirtualdisplay import Display
+
+
 display = Display(visible=0, size=(1920, 1080))  
 display.start()
 
@@ -25,42 +27,17 @@ options.add_argument("--headless") #无界面
 
 
 
-urls = [
-    'https://twitter.com/whyyoutouzhele',
-]
+url = "https://twitter.com/whyyoutouzhele"
+driver = webdriver.Chrome(options=options)
+driver.get(url)
+time.sleep(6)
 
-# create an empty list to store the HTML sources
-html_sources = []
+end_time = time.time() + 35
+while time.time() < end_time:
+    driver.execute_script("window.scrollBy(0, 40);")
+    time.sleep(0.5)
 
-
-# iterate over the URLs and get the HTML source
-for url in urls:
-    try:
-        driver = webdriver.Chrome(options=options)
-        driver.set_page_load_timeout(20)
-        driver.get(url)
-        time.sleep(10)
-        end_time = time.time() + 15
-        while time.time() < end_time:
-         driver.execute_script("window.scrollBy(0, 100);")
-         time.sleep(0.5)
-        driver.quit()
-    except TimeoutException:
-        driver = webdriver.Chrome(options=options)
-        driver.set_page_load_timeout(20)
-        driver.get(url)
-        time.sleep(10)
-        end_time = time.time() + 15
-        while time.time() < end_time:
-         driver.execute_script("window.scrollBy(0, 100);")
-         time.sleep(0.5)
-        driver.quit()
-    except Exception as e:
-        print(f"Error getting HTML for URL {url}: {e}")
-
-
-# join the HTML sources with two newline characters
 html = driver.page_source
-print (html)
+print(html)
 exit()
 
