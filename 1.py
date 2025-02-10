c213a365247e91313dad461d138e8ecdbaf5c4d0
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2025-02-10 18:19:34 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
+import time
+import re
+from datetime import datetime
 from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))  
 display.start()
@@ -10,31 +13,20 @@ chromedriver_autoinstaller.install()  # Check if the current version of chromedr
                                       # and if it doesn't exist, download it automatically,
                                       # then add chromedriver to path
 
-chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
-options = [
-  # Define window size here
-   "--window-size=1200,1200",
-    "--ignore-certificate-errors"
- 
-    #"--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-]
+options = webdriver.ChromeOptions()
 
-for option in options:
-    chrome_options.add_argument(option)
+options.add_argument('--enable-javascript') # 启用 JavaScript
+options.add_argument('blink-settings=imagesEnabled=false')      # 不加载图片，提升运行速度
+options.add_argument('--no-sandbox')                # 解决DevToolsActivePort文件不存在的报错
+options.add_argument('--disable-gpu')               # 谷歌文档提到需要加上这个属性来规避bug
+options.add_argument('--hide-scrollbars')           # 隐藏滚动条，应对一些特殊页面
 
-    
-driver = webdriver.Chrome(options = chrome_options)
+
+driver = webdriver.Chrome(options=options)
 
 driver.get('http://github.com')
 print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
+with open('./GitHub_Action_Results.txt', 'w', encoding='utf-8') as f:
     f.write(f"This was written with a GitHub action {driver.title}")
+
 
